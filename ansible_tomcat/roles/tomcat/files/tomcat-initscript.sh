458224f92025345bd47a07e894af30814308e5c3
Stephane AMOUDI <samodev1725@gmail.com>
2018-09-27 09:44:02 +0200

    modification du dossier files de tomcat pour ajuster le chemin de java

--- ansible_tomcat/roles/tomcat/files/tomcat-initscript.sh
+++ ansible_tomcat/roles/tomcat/files/tomcat-initscript.sh
@@ -15,7 +15,7 @@
 #
  
 #Location of JAVA_HOME (bin files)
-export JAVA_HOME=/usr/bin/java
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
  
 #Add Java binary files to PATH
 export PATH=$JAVA_HOME/bin:$PATH
